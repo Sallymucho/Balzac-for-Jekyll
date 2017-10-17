61f1983bc00dc05bc94463e928023f1aeadafdaa
Louise Wiley <31670921+Sallymucho@users.noreply.github.com>
2017-10-17 13:13:53 -0400

    Add files via upload

---
+++ Hill (2).py
@@ -0,0 +1,66 @@
+print("Hey! I 'm Hillary bla bla, Is it your friends birthday?")
+ 
+firstR = input('Yes or No?' )
+ 
+if(firstR == 'Yes') or (firstR == 'yes'):
+
+
+	secondR = input('What is their name? ')
+         	
+	thirdR = input ('rad! Are you looking for Ideas on getting a gift? Or throwing a Party? ')
+ 
+	if(thirdR== 'Gift') or (firstR == 'gift'):
+ 
+		print("Once I found a photo of me with a friend and gave it to him for a surprise 50th birthday! That was a great gift idea. Also never underestimate the value of a call. I make sure to add John Kerry to my call list whenever his birthday comes around. If you don't have the time to make the call yourself, have your assistant do it on your behalf")
+ 
+	if(thirdR == 'Party') or (thirdR == 'party') :
+ 
+		print('Interested in having something HYPE or casual?')
+
+		fourthR = input ('Hype or Casual ')
+ 
+		if(fourthR == 'Hype') or (fourthR == 'hype'):
+ 
+			print('Do what we did for Jen. We had donuts in the morning. We had cake and ice cream later. Also, FYI, do another little "get-together" during lunch and send-off your friend with ice cream or something fun. Then I say do wine and snacks on later in the night.')
+
+
+		if(fourthR == 'Casual' or fourthR == 'casual'):
+ 
+			print('I say bring goodies for them in their honor, leave em in the break room. Otherwise im a big advocate of money donations as gifts')
+
+	print("                   \ `|")
+	print("                    ) (   .-""-.")
+	print("                    | |  /_  {  '.")
+	print("                    | | (/ `\   } )")
+	print("                    | |  ^/ ^`}   { ")
+	print("                    \  \ \=  ( {   )")
+	print("                     \  \ '-, {   {{")
+	print("                      \  \_.'  ) }  )")
+	print("                       \.-'   (     (")
+	print("                       /'-. _. ) (  }")
+	print("                       \_(    {   _/")
+	print("                           '-- "  )
+	print("                    _.-'       /  / /")
+	print("             <\/>_.'         .'  / /")
+	print("         <\/></\>/.  '      /<\// /")
+	print("         </\>  _ |\`- _ . -/|<// (")
+	print("      <\/>    - _- `  _.-'`_/- |  ")
+	print("      </\>        -  - -  -     ")
+	print("       }`<\/>                <\/>`{")
+	print("       { </\>-<\/>_<\/>_<\/>-</\> }")
+	print(" }      </\> </\> </\>      {")
+	print("     <\/>.                         <\/>")
+	print("     </\>                          </\>")
+	print("      {`<\/>                     <\/>`}")
+	print("      } </\>-<\/>_<\/>_<\/>_<\/>-</\> {")
+	print("      {      </\> </\> </\> </\>      }")
+	print("      }                               }")
+	print("      {           H A P P Y           {")
+	print("   <\/>        B I R T H D A Y        <\/>")
+	print("  </\>                               </\>")
+	print("     `<\/>                          <\/>'")
+	print("  	 </\>-<\/>_<\/>_<\/>_<\/>_<\/>-</\>")
+	print("         ")
+	print("          </\> </\> </\> </\> </\>")
+ 
+else : print("too bad I love birthdays, goodbye")
